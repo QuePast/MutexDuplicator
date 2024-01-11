15af67c4dc9d64f5c29310b1a6380df6b428a3f7
Que <8728328+QuePast@users.noreply.github.com>
2024-01-11 18:35:02 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MutexKiller
+{
+    class Program
+    {
+        private static readonly string BaseMutexName = @"\Sessions\1\BaseNamedObjects\usbhub_Perf_Library_Lock_PlD";
+
+        static void Main()
+        {
+            try
+            {
+                DuplicateMutexHandles();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+
+        private static void DuplicateMutexHandles()
+        {
+            Process notepadProcess = Process.GetProcessesByName("python").FirstOrDefault();
+            if (notepadProcess == null)
+            {
+                Console.WriteLine("python.exe is not running.");
+                return;
+            }
+
+            var leagueProcesses = Process.GetProcesses()
+                                         .Where(p => p.ProcessName.StartsWith("Leagueb"))
+                                         .ToList();
+
+            foreach (var process in leagueProcesses)
+            {
+                Console.WriteLine("Checking in Process: " + process.ProcessName);
+
+                List<Win32API.SYSTEM_HANDLE_INFORMATION> handles = Win32Processes.GetHandles(process, "Mutant");
+                foreach (var handle in handles)
+                {
+                    string objectName = Win32Processes.GetObjectName(handle, process);
+                    if (!string.IsNullOrWhiteSpace(objectName) && objectName.StartsWith(BaseMutexName))
+                    {
+                        if (DuplicateHandleToPython(notepadProcess, process, handle))
+                        {
+                            Console.WriteLine($"Duplicated handle from {process.ProcessName} to python.exe.");
+                        }
+                    }
+                }
+            }
+        }
+
+        private static bool DuplicateHandleToPython(Process notepadProcess, Process sourceProcess, Win32API.SYSTEM_HANDLE_INFORMATION handle)
+        {
+            IntPtr duplicatedHandle;
+            return Win32API.DuplicateHandle(
+                sourceProcess.Handle,
+                handle.Handle,
+                notepadProcess.Handle,
+                out duplicatedHandle,
+                0,
+                false,
+                Win32API.DUPLICATE_SAME_ACCESS);
+        }
+    }
+}
---
+++ Win32Processes.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace MutexKiller
+{
+    public class Win32Processes
+    {
+        const int CNST_SYSTEM_HANDLE_INFORMATION = 16;
+        const uint STATUS_INFO_LENGTH_MISMATCH = 0xc0000004;
+
+        public static string GetObjectTypeName(Win32API.SYSTEM_HANDLE_INFORMATION shHandle, Process process)
+        {
+            IntPtr m_ipProcessHwnd = Win32API.OpenProcess(Win32API.ProcessAccessFlags.All, false, process.Id);
+            IntPtr ipHandle = IntPtr.Zero;
+            var objBasic = new Win32API.OBJECT_BASIC_INFORMATION();
+            IntPtr ipBasic = IntPtr.Zero;
+            var objObjectType = new Win32API.OBJECT_TYPE_INFORMATION();
+            IntPtr ipObjectType = IntPtr.Zero;
+            IntPtr ipObjectName = IntPtr.Zero;
+            string strObjectTypeName = "";
+            int nLength = 0;
+            int nReturn = 0;
+            IntPtr ipTemp = IntPtr.Zero;
+
+            if (!Win32API.DuplicateHandle(m_ipProcessHwnd, shHandle.Handle,
+                                          Win32API.GetCurrentProcess(), out ipHandle,
+                                          0, false, Win32API.DUPLICATE_SAME_ACCESS))
+                return null;
+
+            ipBasic = Marshal.AllocHGlobal(Marshal.SizeOf(objBasic));
+            Win32API.NtQueryObject(ipHandle, (int)Win32API.ObjectInformationClass.ObjectBasicInformation,
+                                   ipBasic, Marshal.SizeOf(objBasic), ref nLength);
+            objBasic = (Win32API.OBJECT_BASIC_INFORMATION)Marshal.PtrToStructure(ipBasic, objBasic.GetType());
+            Marshal.FreeHGlobal(ipBasic);
+
+            ipObjectType = Marshal.AllocHGlobal(objBasic.TypeInformationLength);
+            nLength = objBasic.TypeInformationLength;
+            while ((uint)(nReturn = Win32API.NtQueryObject(
+                ipHandle, (int)Win32API.ObjectInformationClass.ObjectTypeInformation, ipObjectType,
+                  nLength, ref nLength)) ==
+                Win32API.STATUS_INFO_LENGTH_MISMATCH)
+            {
+                Marshal.FreeHGlobal(ipObjectType);
+                ipObjectType = Marshal.AllocHGlobal(nLength);
+            }
+
+            objObjectType = (Win32API.OBJECT_TYPE_INFORMATION)Marshal.PtrToStructure(ipObjectType, objObjectType.GetType());
+            if (Is64Bits())
+            {
+                ipTemp = new IntPtr(Convert.ToInt64(objObjectType.Name.Buffer.ToString(), 10) >> 32);
+            }
+            else
+            {
+                ipTemp = objObjectType.Name.Buffer;
+            }
+
+            strObjectTypeName = Marshal.PtrToStringUni(ipTemp, objObjectType.Name.Length >> 1);
+            Marshal.FreeHGlobal(ipObjectType);
+            return strObjectTypeName;
+        }
+
+        public static string GetObjectName(Win32API.SYSTEM_HANDLE_INFORMATION shHandle, Process process)
+        {
+            IntPtr m_ipProcessHwnd = Win32API.OpenProcess(Win32API.ProcessAccessFlags.All, false, process.Id);
+            IntPtr ipHandle = IntPtr.Zero;
+            var objBasic = new Win32API.OBJECT_BASIC_INFORMATION();
+            IntPtr ipBasic = IntPtr.Zero;
+            IntPtr ipObjectType = IntPtr.Zero;
+            var objObjectName = new Win32API.OBJECT_NAME_INFORMATION();
+            IntPtr ipObjectName = IntPtr.Zero;
+            string strObjectName = "";
+            int nLength = 0;
+            int nReturn = 0;
+            IntPtr ipTemp = IntPtr.Zero;
+
+            if (!Win32API.DuplicateHandle(m_ipProcessHwnd, shHandle.Handle, Win32API.GetCurrentProcess(),
+                                          out ipHandle, 0, false, Win32API.DUPLICATE_SAME_ACCESS))
+                return null;
+
+            ipBasic = Marshal.AllocHGlobal(Marshal.SizeOf(objBasic));
+            Win32API.NtQueryObject(ipHandle, (int)Win32API.ObjectInformationClass.ObjectBasicInformation,
+                                   ipBasic, Marshal.SizeOf(objBasic), ref nLength);
+            objBasic = (Win32API.OBJECT_BASIC_INFORMATION)Marshal.PtrToStructure(ipBasic, objBasic.GetType());
+            Marshal.FreeHGlobal(ipBasic);
+
+
+            nLength = objBasic.NameInformationLength;
+
+            ipObjectName = Marshal.AllocHGlobal(nLength);
+            while ((uint)(nReturn = Win32API.NtQueryObject(
+                     ipHandle, (int)Win32API.ObjectInformationClass.ObjectNameInformation,
+                     ipObjectName, nLength, ref nLength))
+                   == Win32API.STATUS_INFO_LENGTH_MISMATCH)
+            {
+                Marshal.FreeHGlobal(ipObjectName);
+                ipObjectName = Marshal.AllocHGlobal(nLength);
+            }
+            objObjectName = (Win32API.OBJECT_NAME_INFORMATION)Marshal.PtrToStructure(ipObjectName, objObjectName.GetType());
+
+            if (Is64Bits())
+            {
+                ipTemp = new IntPtr(Convert.ToInt64(objObjectName.Name.Buffer.ToString(), 10) >> 32);
+            }
+            else
+            {
+                ipTemp = objObjectName.Name.Buffer;
+            }
+
+            if (ipTemp != IntPtr.Zero)
+            {
+
+                byte[] baTemp2 = new byte[nLength];
+                try
+                {
+                    Marshal.Copy(ipTemp, baTemp2, 0, nLength);
+
+                    strObjectName = Marshal.PtrToStringUni(Is64Bits() ?
+                                                           new IntPtr(ipTemp.ToInt64()) :
+                                                           new IntPtr(ipTemp.ToInt32()));
+                    return strObjectName;
+                }
+                catch (AccessViolationException)
+                {
+                    return null;
+                }
+                finally
+                {
+                    Marshal.FreeHGlobal(ipObjectName);
+                    Win32API.CloseHandle(ipHandle);
+                }
+            }
+            return null;
+        }
+
+        public static List<Win32API.SYSTEM_HANDLE_INFORMATION>
+        GetHandles(Process process = null, string IN_strObjectTypeName = null, string IN_strObjectName = null)
+        {
+            uint nStatus;
+            int nHandleInfoSize = 0x10000;
+            IntPtr ipHandlePointer = Marshal.AllocHGlobal(nHandleInfoSize);
+            int nLength = 0;
+            IntPtr ipHandle = IntPtr.Zero;
+
+            while ((nStatus = Win32API.NtQuerySystemInformation(CNST_SYSTEM_HANDLE_INFORMATION, ipHandlePointer,
+                                                                nHandleInfoSize, ref nLength)) ==
+                    STATUS_INFO_LENGTH_MISMATCH)
+            {
+                nHandleInfoSize = nLength;
+                Marshal.FreeHGlobal(ipHandlePointer);
+                ipHandlePointer = Marshal.AllocHGlobal(nLength);
+            }
+
+            byte[] baTemp = new byte[nLength];
+            Marshal.Copy(ipHandlePointer, baTemp, 0, nLength);
+
+            long lHandleCount = 0;
+            if (Is64Bits())
+            {
+                lHandleCount = Marshal.ReadInt64(ipHandlePointer);
+                ipHandle = new IntPtr(ipHandlePointer.ToInt64() + 8);
+            }
+            else
+            {
+                lHandleCount = Marshal.ReadInt32(ipHandlePointer);
+                ipHandle = new IntPtr(ipHandlePointer.ToInt32() + 4);
+            }
+
+            Win32API.SYSTEM_HANDLE_INFORMATION shHandle;
+            List<Win32API.SYSTEM_HANDLE_INFORMATION> lstHandles = new List<Win32API.SYSTEM_HANDLE_INFORMATION>();
+
+            for (long lIndex = 0; lIndex < lHandleCount; lIndex++)
+            {
+                shHandle = new Win32API.SYSTEM_HANDLE_INFORMATION();
+                if (Is64Bits())
+                {
+                    shHandle = (Win32API.SYSTEM_HANDLE_INFORMATION)Marshal.PtrToStructure(ipHandle, shHandle.GetType());
+                    ipHandle = new IntPtr(ipHandle.ToInt64() + Marshal.SizeOf(shHandle) + 8);
+                }
+                else
+                {
+                    ipHandle = new IntPtr(ipHandle.ToInt64() + Marshal.SizeOf(shHandle));
+                    shHandle = (Win32API.SYSTEM_HANDLE_INFORMATION)Marshal.PtrToStructure(ipHandle, shHandle.GetType());
+                }
+
+                if (process != null)
+                {
+                    if (shHandle.ProcessID != process.Id) continue;
+                }
+
+                string strObjectTypeName = "";
+                if (IN_strObjectTypeName != null)
+                {
+                    strObjectTypeName = GetObjectTypeName(shHandle, Process.GetProcessById(shHandle.ProcessID));
+                    if (strObjectTypeName != IN_strObjectTypeName) continue;
+                }
+
+                string strObjectName = "";
+                if (IN_strObjectName != null)
+                {
+                    strObjectName = GetObjectName(shHandle, Process.GetProcessById(shHandle.ProcessID));
+                    if (strObjectName != IN_strObjectName) continue;
+                }
+
+                string strObjectTypeName2 = GetObjectTypeName(shHandle, Process.GetProcessById(shHandle.ProcessID));
+                string strObjectName2 = GetObjectName(shHandle, Process.GetProcessById(shHandle.ProcessID));
+                Console.WriteLine("{0}   {1}   {2}", shHandle.ProcessID, strObjectTypeName2, strObjectName2);
+
+                lstHandles.Add(shHandle);
+            }
+            return lstHandles;
+        }
+
+        public static bool Is64Bits()
+        {
+            return Marshal.SizeOf(typeof(IntPtr)) == 8 ? true : false;
+        }
+    }
+}
